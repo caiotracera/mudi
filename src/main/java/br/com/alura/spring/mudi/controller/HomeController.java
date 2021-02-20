015276e22dea4d03afd9c74150f405278b2db066
Caio Tracera <ctrcra@gmail.com>
2021-02-20 15:02:03 -0300

    feat: add request by pedido's status

--- src/main/java/br/com/alura/spring/mudi/controller/HomeController.java
+++ src/main/java/br/com/alura/spring/mudi/controller/HomeController.java
@@ -1,26 +1,46 @@
 package br.com.alura.spring.mudi.controller;
 
 import br.com.alura.spring.mudi.model.Pedido;
+import br.com.alura.spring.mudi.model.StatusPedido;
 import br.com.alura.spring.mudi.repository.PedidoRepository;
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/home")
 public class HomeController {
 
   @Autowired
   private PedidoRepository pedidoRepository;
 
-  @GetMapping("/home")
+  @GetMapping
   public ModelAndView home(Model model) {
     List<Pedido> pedidos = pedidoRepository.findAll();
     ModelAndView mv = new ModelAndView("home");
     mv.addObject("pedidos", pedidos);
 
     return mv;
+  }
+
+  @GetMapping("/{status}")
+  public ModelAndView porStatus(@PathVariable("status") String status, Model model) {
+    List<Pedido> pedidos = pedidoRepository.findByStatus(StatusPedido.valueOf(status.toUpperCase()));
+    ModelAndView mv = new ModelAndView("home");
+    mv.addObject("pedidos", pedidos);
+
+    return mv;
+  }
+
+  @ExceptionHandler(IllegalArgumentException.class)
+  public String onError() {
+    return "redirect:/home";
   }
 }
--- src/main/java/br/com/alura/spring/mudi/repository/PedidoRepository.java
+++ src/main/java/br/com/alura/spring/mudi/repository/PedidoRepository.java
@@ -1,8 +1,16 @@
 package br.com.alura.spring.mudi.repository;
 
 import br.com.alura.spring.mudi.model.Pedido;
+import br.com.alura.spring.mudi.model.StatusPedido;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PedidoRepository extends JpaRepository<Pedido, Long> {}
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+
+	List<Pedido> findByStatus(StatusPedido status);
+
+}
