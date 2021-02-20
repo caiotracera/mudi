60e0b511fb349097770dbf93f0b5b1bf8dce53a4
Caio Tracera <ctrcra@gmail.com>
2021-02-20 14:16:27 -0300

    refactor: add prettier rules

--- src/main/java/br/com/alura/spring/mudi/controller/PedidoController.java
+++ src/main/java/br/com/alura/spring/mudi/controller/PedidoController.java
@@ -1,5 +1,8 @@
 package br.com.alura.spring.mudi.controller;
 
+import br.com.alura.spring.mudi.dto.RequisicaoNovoPedido;
+import br.com.alura.spring.mudi.model.Pedido;
+import br.com.alura.spring.mudi.repository.PedidoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -7,10 +10,6 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import br.com.alura.spring.mudi.dto.RequisicaoNovoPedido;
-import br.com.alura.spring.mudi.model.Pedido;
-import br.com.alura.spring.mudi.repository.PedidoRepository;
 
 @Controller
 @RequestMapping("pedido")
@@ -25,9 +24,12 @@ public class PedidoController {
   }
 
   @PostMapping("novo")
-  public String novo(@Validated RequisicaoNovoPedido requisicao, BindingResult result) {
-    if(result.hasErrors()) {
-      return "pedido/formulario"; 
+  public String novo(
+    @Validated RequisicaoNovoPedido requisicao,
+    BindingResult result
+  ) {
+    if (result.hasErrors()) {
+      return "pedido/formulario";
     }
 
     Pedido pedido = requisicao.toPedido();
